1e670e691bd87c757e2e0390e4d2df31dceb0d2e
Narendra Maurya <amanlearntoenvent17@gmail.com>
2024-04-17 12:29:36 +0530

    fix all bugs and user registered successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     // check if user already exist: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{username}, {email}]
     })
 
@@ -31,7 +31,12 @@ const registerUser = asyncHandler( async (req, res) => {
 
     // check for cover images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) &&
+     req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar Image is required")
